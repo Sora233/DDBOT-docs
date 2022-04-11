06da26f1ef3aa7fbc1cc475aec40b3431683ecef
sora233 <sora@sora233.me>
2022-04-12 01:37:25 +0800

    init commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  base: '/DDBOT',
+  title: 'DDBOT 文档',
+  description: 'DDBOT 文档中心',
+  themeConfig: {
+    repo: 'Sora233/DDBOT-docs',
+    repoLabel: '查看源码',
+    lastUpdated: '最后更新',
+    editLinks: true,
+    editLinkText: '发现问题，救救SORA，我来改正此页面！',
+    smoothScroll: true,
+    sidebar: 'auto',
+    displayAllHeaders: true,
+    tip: "提示",
+    warning: "注意",
+    danger: "警告",
+    backToHome: "返回首页",
+    nav: [
+      {text: '主页', link: '/'},
+      {text: '命令指南', link: '/guide/'},
+      {text: '部署指南', link: '/deploy/'},
+      {text: '模板', link: '/template/'},
+    ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroText: DDBOT
+tagline: 专注于QQ群推送的机器人
+actionText: 快速上手 →
+actionLink: /guide/
+features:
+
+- title: 简洁至上
+  details: 交互被刻意设计成最小程度，群聊不会变成机器聊
+- title: 一键部署
+  details: 不配环境，双击运行
+  footer: CC-BY-SA-4.0 Licensed | Copyright © 2020-present Sora233
+
+---
